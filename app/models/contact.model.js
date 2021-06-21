db44b2a41cec0464092d2437ad166016fb94a390
Krishan Mohan <krishan.mohan@kiwitech.com>
2021-06-21 19:17:56 +0530

    Add email vaildation

--- app/models/contact.model.js
+++ app/models/contact.model.js
@@ -1,12 +1,38 @@
 const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate-v2');
 
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
 
 const ContactSchema = mongoose.Schema({
-    first_name: String,
-    last_name: String,
-    email: String,
-    mobile: Number,
+    first_name: {
+        type: String,
+        trim: true,
+        required: true,
+        maxLength: 50
+    },
+    last_name: {
+        type: String,
+        trim: true,
+        required: true,
+        maxLength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        maxLength: 100,
+        validate: [validateEmail, "Please provide a valid email address."],
+        unique: [true, "This email already exists."]
+    },
+    mobile: {
+        type: Number,
+        required: true,
+        min: 1000000000,
+        max: 9999999999
+    },
 }, {
     timestamps: true
 });
